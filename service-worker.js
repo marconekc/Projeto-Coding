5fc7438aa55f7e5a638c9c5113c2fa0f48b2ceeb
marconekc <kaikeds8@gmail.com>
2023-03-12 23:04:17 -0300

    Ajustes no script de registro do service worker, correção dos iframes, correção do manifest

--- service-worker.js
+++ service-worker.js
@@ -6,8 +6,8 @@ self.addEventListener('install', event => {
 
   event.waitUntil(
     caches.open(cacheName)
-      .then(cache => cache.addAll([
-
+      .then(caches => caches.addAll([
+        './',
         './index.html',
 
         './assets/css/fontawesome-all.min.css',
@@ -17,40 +17,20 @@ self.addEventListener('install', event => {
         './assets/js/browser.min.js',
         './assets/js/jquery.min.js',
         './assets/js/jquery.scrollex.min.js',
-        './assets/css/jquery.scrolly.min.js',
+        './assets/js/jquery.scrolly.min.js',
+        './assets/js/main.js',
+        './assets/js/util.js',
 
         './assets/img/icons/icon_72.png',
         './assets/img/icons/icon_144.png',
         './assets/img/icons/icon_192.png',
         './assets/img/icons/icon_512.png',
         './assets/img/icons/icon_48.png',
-        './assets/img/icons/icon_96.png',
-        './assets/img/icons/icon_16.png',
-        './assets/img/icons/icon_20.png',
-        './assets/img/icons/icon_29.png',
-        './assets/img/icons/icon_32.png',
-        './assets/img/icons/icon_40.png',
-        './assets/img/icons/icon_50.png',
-        './assets/img/icons/icon_57.png',
-        './assets/img/icons/icon_58.png',
-        './assets/img/icons/icon_60.png',
-        './assets/img/icons/icon_64.png',
-        './assets/img/icons/icon_76.png',
-        './assets/img/icons/icon_80.png',
-        './assets/img/icons/icon_87.png',
-        './assets/img/icons/icon_100.png',
-        './assets/img/icons/icon_114.png',
-        './assets/img/icons/icon_120.png',
-        './assets/img/icons/icon_128.png',
-        './assets/img/icons/icon_152.png',
         './assets/img/icons/icon_167.png',
         './assets/img/icons/icon_180.png',
         './assets/img/icons/icon_256.png',
         './assets/img/icons/icon_1024.png',
         
-        './assets/img/imgWF.webp',
-        './assets/img/imFGO.webp',
-        './assets/img/imgGI.webp',
       ]))
   );
 });
